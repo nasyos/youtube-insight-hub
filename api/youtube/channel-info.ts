1975cadcef7e35dde057d3ca8d9118ff757b454e
Yoshi <yoshio.nasu@extool.co.jp>
2025-12-31 13:43:54 +0900

    fix: channel-info.tsのimportをtype importに変更してVercelの認識を改善

--- api/youtube/channel-info.ts
+++ api/youtube/channel-info.ts
@@ -1,4 +1,4 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 const YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3';
 
